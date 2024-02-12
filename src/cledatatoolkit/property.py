ab52622a3136f37e6ea1132d7a4f17bb16db7a9f
Dro Sohrabian <dsohrabian@clevelandohio.gov>
2024-02-12 10:55:22 -0500

    Add Cleveland Clinic to major names for corporate owner regex

--- src/cledatatoolkit/property.py
+++ src/cledatatoolkit/property.py
@@ -5,7 +5,7 @@ import pandas as pd
 biz_flag_re = r"(?i) ?l\.? ?l\.? ?c|\Winco?|\Wcorp|\slp|l-?t-?d|roth ira|limited|\Wtrs\W|-?tru?st?|renovations|liability|resource|enterprise|associ?a?t?|acquisition|comi?pany|\Wco$|llp|\Wl\.?p\.?\W?|management|financial|development|network|invest|co-t|construct|property|properties|solution|buil(der)?|real estate|services|realty|partners?|li?m?i?te?d"
 
 # This captures other special corp names. To be consolidated with main pattern some time.
-major_names_re = r"(?i)Sherwin Williams ?C?o?|iplan group"
+major_names_re = r"(?i)Sherwin Williams ?C?o?|iplan group|Cleveland Clinic"
 
 # Exclusions we want to explicitly define to make sure they are not considered "corporate"
 exclude_re = r"(?i)clev?e?l?a?n?d? elec?|land reutilization|fairfax rennaisance|fairfax homes|university circle,? inc"
