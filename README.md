76989b92048110f3748fc77a0f79097a8886108c
Dro Sohrabian <dsohrabian@clevelandohio.gov>
2024-02-11 23:55:14 -0500

    Finish property module, workable draft of README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # cledatatoolkit
-Library of tools published by City of Cleveland Urban Analytics for civic data analysis
+Library of tools published by City of Cleveland's Office of Urban Analytics, meant for for civic data analysis using local, county and regional data sources, including datasets Cleveland's new Open Data Portal.
+
+## Installation
+You can use `pip` in your terminal to install Urban AI's package.
+
+```
+pip install cle-data-toolkit
+```
+This will also install dependencies such as geopandas, pandas, and Jupyter-notebook support.
+
+We recommending installing into a [virtual environment](https://docs.python.org/3/library/venv.html) to not modify your base version of Python.
+
+## Features
+
+### Spatial Helper Functions
+* Apportioning: Allocating Census data to local boundaries that don't align
+* Custom functions built on top of `geopandas` for more advanced spatial joins
+    * Largest overlap
+### County Property Data Enhancement
+* Extracting insights from property owner names
+* Standardizing property types and ownership
+
+## Guide
+See our tutorial notebook repo, [**open-data-examples**](https://github.com/City-of-Cleveland/open-data-examples), for curated tutorials of how you might use this package with Cleveland civic data sources!
--- src/cledatatoolkit/property.py
+++ src/cledatatoolkit/property.py
@@ -1,4 +1,20 @@
+import pandas as pd
 
 # This regex captures all corporate owners (or business owners) that can be found in deeded_owners
 # Note this pattern is designed for Cuyahoga County's dataset and is not tested on other string matching.
-biz_flag_regex = r"(?i) ?l\.? ?l\.? ?c|\Winco?|\Wcorp|\slp|l-?t-?d|roth ira|limited|\Wtrs\W|-?tru?st?|renovations|liability|resource|enterprise|associ?a?t?|acquisition|comi?pany|\Wco$|llp|\Wl\.?p\.?\W?|management|financial|development|network|invest|co-t|construct|property|properties|solution|buil(der)?|real estate|services|realty|partners?|li?m?i?te?d"
+biz_flag_re = r"(?i) ?l\.? ?l\.? ?c|\Winco?|\Wcorp|\slp|l-?t-?d|roth ira|limited|\Wtrs\W|-?tru?st?|renovations|liability|resource|enterprise|associ?a?t?|acquisition|comi?pany|\Wco$|llp|\Wl\.?p\.?\W?|management|financial|development|network|invest|co-t|construct|property|properties|solution|buil(der)?|real estate|services|realty|partners?|li?m?i?te?d"
+
+# This captures other special corp names. To be consolidated with main pattern some time.
+major_names_re = r"(?i)Sherwin Williams ?C?o?|iplan group"
+
+# Exclusions we want to explicitly define to make sure they are not considered "corporate"
+exclude_re = r"(?i)clev?e?l?a?n?d? elec?|land reutilization|fairfax rennaisance|fairfax homes|university circle,? inc"
+
+def identify_corp_owner(column: pd.Series):
+    owner_column = column.copy()
+    biz_test = owner_column.str.contains(biz_flag_re)
+    major_names_test = owner_column.str.contains(major_names_re)
+    exclude_test = owner_column.str.contains(exclude_re).fillna(False)
+
+    final_flags = (biz_test | major_names_test) & ~(exclude_test)
+    return final_flags
